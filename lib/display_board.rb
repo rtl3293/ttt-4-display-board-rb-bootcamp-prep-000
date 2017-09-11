c82ea53c4312ee757ee7234725902ac1ae38bf33
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-11 18:31:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,21 +3,21 @@
 def display_board(cells)
   border = "|"
   row = ["-----------"]
-  print cells[0]
+  print " #{cells[0]} "
   print border
-  print cells[1]
+  print " #{cells[1]} "
   print border
-  puts cells[2]
+  puts " #{cells[2]} "
   puts row
-  print cells[3]
+  print " #{cells[3]} "
   print border
-  print cells[4]
+  print " #{cells[4]} "
   print border
-  puts cells[5]
+  puts " #{cells[5]} "
   puts row
-  print cells[6]
+  print " #{cells[6]} "
   print border
-  print cells[7]
+  print " #{cells[7]} "
   print border
-  puts cells[8]
+  puts " #{cells[8]} "
 end
