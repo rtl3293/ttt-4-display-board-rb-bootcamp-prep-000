5abada2d03114454d1e494fc00d2794f887f2e24
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-11 18:34:35 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -137,12 +137,18 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Xs' do
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
       # Should you want to write your own test for this situation,
       # read the following code and comments.
-
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Xs?"
-
+      expect(rows[0]).to eq(" X | O | X ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" X | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" X | X | X ")
       # Define the board with values that should create the desired output
       # *** Edit the line below ***
       board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
